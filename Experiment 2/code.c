065586e2666a80e27e56c002fbfed618e19d691d
vaishnavib24 <121717406+vaishnavib24@users.noreply.github.com>
2023-04-28 00:38:48 +0530

    Update code.c

--- Experiment 2/code.c
+++ Experiment 2/code.c
@@ -1,6 +1,125 @@
-#include<stdio.h>
-int main()
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+void merge(int a[], int beg, int mid, int end)
 {
-  printf("Hello");
-  
+    int i, j, k;
+    int n1 = mid - beg + 1;
+    int n2 = end - mid;
+    int LeftArray[n1], RightArray[n2];
+    for (int i = 0; i < n1; i++)
+        LeftArray[i] = a[beg + i];
+    for (int j = 0; j < n2; j++)
+        RightArray[j] = a[mid + 1 + j];
+    i = 0,
+    j = 0;
+    k = beg;
+    while (i < n1 && j < n2)
+    {
+        if (LeftArray[i] <= RightArray[j])
+        {
+            a[k] = LeftArray[i];
+            i++;
+        }
+        else
+        {
+            a[k] = RightArray[j];
+            j++;
+        }
+        k++;
+    }
+    while (i < n1)
+    {
+        a[k] = LeftArray[i];
+        i++;
+        k++;
+    }
+    while (j < n2)
+    {
+        a[k] = RightArray[j];
+        j++;
+        k++;
+    }
+}
+void mergeSort(int a[], int beg, int end)
+{
+    if (beg < end)
+    {
+        int mid = (beg + end) / 2;
+        mergeSort(a, beg, mid);
+        mergeSort(a, mid + 1, end);
+        merge(a, beg, mid, end);
+    }
+}
+void printArray(int a[], int n)
+{
+    int i;
+    for (i = 0; i < n; i++)
+        printf("%d ", a[i]);
+    printf("\n");
+}
+int partition(int a[], int start, int end)
+{
+    int pivot = a[end]; // pivot element
+    int i = (start - 1);
+    for (int j = start; j <= end - 1; j++)
+    {
+        if (a[j] < pivot)
+        {
+            i++;
+            int t = a[i];
+            a[i] = a[j];
+            a[j] = t;
+        }
+    }
+    int t = a[i + 1];
+    a[i + 1] = a[end];
+    a[end] = t;
+    return (i + 1);
+}
+void quick(int a[], int start, int end)
+{
+    if (start < end)
+    {
+        int p = partition(a, start, end);
+        quick(a, start, p - 1);
+        quick(a, p + 1, end);
+    }
+}
+void printArr(int a[], int n)
+{
+    int i;
+    for (i = 0; i < n; i++)
+        printf("%d ", a[i]);
+}
+void main()
+{
+    int n = 0;
+    for (int k = 0; k < (100000 / 100); k++)
+    {
+        n = n + 100;
+        int num[n];
+        int quicksort[n];
+        int merge[n];
+        int j, min;
+        clock_t start_t, end_t;
+        double total_t;
+        printf("%d\t", n);
+        for (int i = 0; i < n; i++)
+        {
+            num[i] = rand() % 10;
+            merge[i] = num[i];
+            quicksort[i] = num[i];
+        }
+        start_t = clock();
+        mergeSort(merge, 0, n - 1);
+        end_t = clock();
+        total_t = (double)(end_t - start_t) / CLOCKS_PER_SEC;
+        printf("%f\n", total_t);
+        start_t = clock();
+        quick(quicksort, 0, n - 1);
+        end_t = clock();
+        total_t = (double)(end_t - start_t) / CLOCKS_PER_SEC;
+        printf("%f\n", total_t);
+    }
 }
